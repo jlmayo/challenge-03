009e920b99f2e01cc9ad571c7a8c5615eb39ff5d
jlmayo <jlmayo22@gmail.com>
2022-08-19 10:46:27 -0700

    Completely rewrote js. Doesn't work.

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 // Assignment code here
 var generateBtn = document.querySelector("#generate");
 
-function randomInt(min, max) {
-  if (!max) {
-    max = min
-    min = 0
-  }
-  var random = Math.random()
-  return Math.floor(min*(1 - random) + random*max)
-}
+var numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbolChoices = ["*", "&", "^", "%", "$", "#", "@", "!", "~", "{", "}", "?", "<", ">", "."]
+var alphaChoicesLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var alphaChoicesUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
 
-function passwordItems(list) {
-  return list[randomInt(0, list.length-1)]
-}
+var passwordLength;
+var userSelectsNum;
+var userSelectsSym;
+var userSelectsLowC;
+var userSelectsUppC;
 
 function generatePassword() {
 
@@ -21,7 +19,7 @@ function generatePassword() {
   var passwordLength = parseInt(userSelect)
 
   if (isNaN(passwordLength)) {
-    window.alert("That is not a number.")
+    window.alert("That is not a number. Please try again.")
     return
   }
 
@@ -34,40 +32,63 @@ function generatePassword() {
   var userSelectsSym = window.confirm("Should this password include symbols?")
   var userSelectsLowC = window.confirm("Should this password include lowercase letters?")
   var userSelectsUppC = window.confirm("Should this password include uppercase letters?")
+  
 
-  var numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var symbolChoices = ["*", "&", "^", "%", "$", "#", "@", "!", "~", "{", "}", "?", "<", ">", "."]
-  var alphaChoicesLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var alphaChoicesUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
-
-  var passwordChoices = []
-
-  if (userSelectsNum) {
-    passwordChoices.push(numberChoices)
+  if (!userSelectsNum && !userSelectsSym && !userSelectsLowC && !userSelectsUppC) {
+    window.alert("You must choose a criteria. Please try again.")
   }
-
-  if (userSelectsSym) {
-    passwordChoices.push(symbolChoices)
+   else if (userSelectsNum && userSelectsSym && userSelectsLowC && userSelectsUppC) {
+    passwordChoices = character.concat(numberChoices, symbolChoices, alphaChoicesLower, alphaChoicesUpper);
   }
-
-  if (userSelectsLowC) {
-    passwordChoices.push(alphaChoicesLower)
+   else if (userSelectsNum && userSelectsSym && userSelectsLowC) {
+    passwordChoices = character.concat(numberChoices, symbolChoices, alphaChoicesLower);
   }
-
-  if (userSelectsUppC) {
-    passwordChoices.push(alphaChoicesUpper)
+   else if (userSelectsNum && userSelectsSym && userSelectsUppC) {
+      passwordChoices = character.concat(numberChoices, symbolChoices, alphaChoicesUpper);
   }
+  else if (userSelectsNum && userSelectsLowC && userSelectsUppC) {
+    passwordChoices = number.concat(numberChoices, alphaChoicesLower, alphaChoicesUpper);
+  }
+  else if (userSelectsSym && userSelectsLowC && userSelectsUppC) {
+    passwordChoices = character.concat(symbolChoices, alphaChoicesLower, alphaChoicesUpper);
+  }
+  else if (userSelectsNum && userSelectsSym) {
+    passwordChoices = character.concat(numberChoices, symbolChoices);
+  }
+  else if (userSelectsNum && userSelectsLowC) {
+    passwordChoices = character.concat(numberChoices, alphaChoicesLower);
+  }
+  else if (userSelectsNum && userSelectsUppC) {
+    passwordChoices = character.concat(numberChoices, alphaChoicesUpper);
+  }
+  else if (userSelectsSym && userSelectsLowC) {
+    passwordChoices = character.concat(symbolChoices, alphaChoicesLower);
+  }
+  else if (userSelectsSym && userSelectsUppC) {
+    passwordChoices = character.concat(symbolChoices, alphaChoicesUpper);
+  }
+  else if (userSelectsNum) {
+    passwordChoices = character.concat(numberChoices);
+  }
+  else if (userSelectsSym && userSelectsLowC) {
+    passwordChoices = character.concat(symbolChoices);
+  }
+  else if (userSelectsLowC) {
+    passwordChoices = character.concat(alphaChoicesLower);
+  }
+  else if (userSelectsUppC) {
+    passwordChoices = character.concat(alphaChoicesUpper);
+  };
 
-  var passwordComplete = ""
-
+  var passwordComplete = [];
 
   for (var i = 0; i < passwordLength; i++) {
-    var passwordResult = passwordItems(passwordChoices)
-    var passwordRandom = passwordItems(randomItems)
-    passwordComplete += passwordRandom
+    var randomList = passwordChoices[Math.floor(Math.random() * passwordChoices.length)];
+    passwordComplete.push(randomList);
   }
-}
 
+ return passwordComplete.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
